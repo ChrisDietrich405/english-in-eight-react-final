cb2e92b44768f4f0017207d41ede2f5fe99f4701
Chris Dietrich <chrisdietrich3666@gmail.com>
2022-01-29 11:04:31 -0500

    backup

--- src/pages/versus/versus-categories/AllVersusEvery.js
+++ src/pages/versus/versus-categories/AllVersusEvery.js
@@ -1,138 +1,151 @@
-import Quiz from "../../../components/Quiz"
+import Quiz from "../../../components/Quiz";
 
 export default function AllVersusEvery() {
-    return (
-            <div className="page-body">
-                <div className="page-title">
-                   <h2>All Versus Every</h2>
-                </div>
-                
-                <p className="grammar-explanation-paragraph">We use both all and every to refer to the total number of something. All refers to a complete group. Every refers to each member of a complete group:</p>
-                <ul className="page-examples">
-                    <li>The questionnaire was sent to all employees.</li>
-                    <li>The questionnaire was sent to every employee.</li>
-                </ul>
-                <p>We can use every to focus on each individual member.</p>
-                <p>Compare:</p>
-                <ul className="page-examples">
-                    <li>All passengers must turn off their cell phones. (refers to the whole group)</li>
-                    <li>Every passenger must turn off their cell phone. (focuses on each individual member of the whole group)</li>
-                </ul>
-                <p>We can use all, but not every, on its own without a noun. We use everyone/everybody/everything instead:</p>
-                <ul className="page-examples">
-                    <li>The meeting is at Oriel Hall. It begins at 8 pm and all are welcome.</li>
-                    <li>The meeting is at Oriel Hall. It begins at 8 pm and everyone is welcome.</li>
-                </ul>
-                <p>We use all with plural and uncountable nouns and every with singular nouns:</p>
-                <ul className="page-examples">
-                    <li>All donations will be sent to the earthquake relief fund.</li>
-                    <li>Every donation is appreciated.</li>
-                </ul>
-                <p>We use all day, all week, all month to mean ‘one entire day/week/month’:</p>
-                <ul className="page-examples">
-                    <li>We spent all day at the beach yesterday.</li>
-                </ul>
-                <p>Every day (week/month) focuses on each individual day (week/month):</p>
-                <ul className="page-examples">
-                    <li>We spent every day at the beach when we were on vacation .</li>
-                </ul>
-            
-            <p>Add all or every</p>
-            <Quiz title="Quiz" questions={[
-                {
-                    title: "_________________ child needs love.",
-                    possibleAnswers: [
-                        {title: "every", correctAnswer: true},
-                        {title: "all"},
-                    ],
-                    userAnswer: "",
-                },
-                {
-                    title: "I have written to ______________ my friends.",
-                    possibleAnswers: [
-                        {title: "every" },
-                        {title: "all", correctAnswer: true},
-                    ],
-                    userAnswer: "",
-                },
-                {
-                    title: "Not ___________ birds can sing.",
-                    possibleAnswers: [
-                        {title: "every" },
-                        {title: "all",correctAnswer: true},
-                    ],
-                    userAnswer: "",
-                },
-                {
-                    title: "I see her ______________ few days.",
-                    possibleAnswers: [
-                        {title: "all" },
-                        {title: "every",correctAnswer: true},
-                    ],
-                    userAnswer: "",
-                },
-                {
-                    title: " I have cleaned _______________ the rooms except the bathroom.",
-                    possibleAnswers: [
-                        {title: "all", correctAnswer: true },
-                        {title: "every"},
-                    ],
-                    userAnswer: "",
-                },
-                {
-                    title: " You don’t see tigers __________ day.",
-                    possibleAnswers: [
-                        {title: "all" },
-                        {title: "every", correctAnswer: true },
-                    ],
-                    userAnswer: "",
-                },
-                {
-                    title: "I have invited ____________ my friends.",
-                    possibleAnswers: [
-                        {title: "all", correctAnswer: true },
-                        {title: "every" },
-                    ],
-                    userAnswer: "",
-                },
-                {
-                    title: "I see my dentist ______________ six months.",
-                    possibleAnswers: [
-                        {title: "every", correctAnswer: true },
-                        {title: "all" },
-                    ],
-                    userAnswer: "",
-                },
-                {
-                    title: " I talk to my grandparents ____________ day.",
-                    possibleAnswers: [
-                        {title: "every", correctAnswer: true },
-                        {title: "all" },
-                    ],
-                    userAnswer: "",
-                },
-                {
-                    title: "I’ve worked for this company ___________ my life.",
-                    possibleAnswers: [
-                        {title: "every" },
-                        {title: "all", correctAnswer: true },
-                    ],
-                    userAnswer: "",
-                },       
-            ]}></Quiz>
-        </div>
-    )
-} 
+  return (
+    <div className="page-body">
+      <div className="page-title">
+        <h2>All Versus Every</h2>
+      </div>
 
+      <p className="grammar-explanation-paragraph">
+        We use both all and every to refer to the total number of something. All
+        refers to a complete group. Every refers to each member of a complete
+        group:
+      </p>
+      <ul className="page-examples">
+        <li>The questionnaire was sent to all employees.</li>
+        <li>The questionnaire was sent to every employee.</li>
+      </ul>
+      <p>We can use every to focus on each individual member.</p>
+      <p>Compare:</p>
+      <ul className="page-examples">
+        <li>
+          All passengers must turn off their cell phones. (refers to the whole
+          group)
+        </li>
+        <li>
+          Every passenger must turn off their cell phone. (focuses on each
+          individual member of the whole group)
+        </li>
+      </ul>
+      <p>
+        We can use all, but not every, on its own without a noun. We use
+        everyone/everybody/everything instead:
+      </p>
+      <ul className="page-examples">
+        <li>
+          The meeting is at Oriel Hall. It begins at 8 pm and all are welcome.
+        </li>
+        <li>
+          The meeting is at Oriel Hall. It begins at 8 pm and everyone is
+          welcome.
+        </li>
+      </ul>
+      <p>
+        We use all with plural and uncountable nouns and every with singular
+        nouns:
+      </p>
+      <ul className="page-examples">
+        <li>All donations will be sent to the earthquake relief fund.</li>
+        <li>Every donation is appreciated.</li>
+      </ul>
+      <p>
+        We use all day, all week, all month to mean ‘one entire day/week/month’:
+      </p>
+      <ul className="page-examples">
+        <li>We spent all day at the beach yesterday.</li>
+      </ul>
+      <p>Every day (week/month) focuses on each individual day (week/month):</p>
+      <ul className="page-examples">
+        <li>We spent every day at the beach when we were on vacation .</li>
+      </ul>
 
-
-
-
-
-
-
-
-
-
-
-
+      <Quiz
+        title="Take a Quiz!"
+        questions={[
+          {
+            title: "_________________ child needs love.",
+            possibleAnswers: [
+              { title: "every", correctAnswer: true },
+              { title: "all" },
+            ],
+            userAnswer: "",
+          },
+          {
+            title: "I have written to ______________ my friends.",
+            possibleAnswers: [
+              { title: "every" },
+              { title: "all", correctAnswer: true },
+            ],
+            userAnswer: "",
+          },
+          {
+            title: "Not ___________ birds can sing.",
+            possibleAnswers: [
+              { title: "every" },
+              { title: "all", correctAnswer: true },
+            ],
+            userAnswer: "",
+          },
+          {
+            title: "I see her ______________ few days.",
+            possibleAnswers: [
+              { title: "all" },
+              { title: "every", correctAnswer: true },
+            ],
+            userAnswer: "",
+          },
+          {
+            title:
+              " I have cleaned _______________ the rooms except the bathroom.",
+            possibleAnswers: [
+              { title: "all", correctAnswer: true },
+              { title: "every" },
+            ],
+            userAnswer: "",
+          },
+          {
+            title: " You don’t see tigers __________ day.",
+            possibleAnswers: [
+              { title: "all" },
+              { title: "every", correctAnswer: true },
+            ],
+            userAnswer: "",
+          },
+          {
+            title: "I have invited ____________ my friends.",
+            possibleAnswers: [
+              { title: "all", correctAnswer: true },
+              { title: "every" },
+            ],
+            userAnswer: "",
+          },
+          {
+            title: "I see my dentist ______________ six months.",
+            possibleAnswers: [
+              { title: "every", correctAnswer: true },
+              { title: "all" },
+            ],
+            userAnswer: "",
+          },
+          {
+            title: " I talk to my grandparents ____________ day.",
+            possibleAnswers: [
+              { title: "every", correctAnswer: true },
+              { title: "all" },
+            ],
+            userAnswer: "",
+          },
+          {
+            title: "I’ve worked for this company ___________ my life.",
+            possibleAnswers: [
+              { title: "every" },
+              { title: "all", correctAnswer: true },
+            ],
+            userAnswer: "",
+          },
+        ]}
+      ></Quiz>
+    </div>
+  );
+}
--- src/pages/versus/versus-categories/RememberVersusRemind.js
+++ src/pages/versus/versus-categories/RememberVersusRemind.js
@@ -1,134 +1,159 @@
-import Quiz from "../../../components/Quiz"
+import Quiz from "../../../components/Quiz";
 
 export default function RememberVersusRemind() {
-    return (
-            <div className="page-body">
-                <div className="page-title">
-                   <h2>Remember versus Remind</h2>
-                </div>
-                <h3>Remember</h3>
-                <p>If we remember someone or something, we keep that person or thing in our mind or we bring that person or thing back to our mind:</p>
-                <ul className="page-examples">
-                    <li>I’ll always remember you. You have helped me so much.</li>
-                    <li>Suddenly I remembered I promised to call my mother.</li>
-                </ul>
-                <p>Remember + infinitive means that we don’t forget to do something:</p>
-                <ul className="page-examples">
-                    <li>Remember to back up your files, just in case your computer crashes.</li>
-                </ul>
-                <p>Remember + the -ing form of a verb means ‘have a memory of something we did or of something that happened in the past:</p>
-                <ul className="page-examples">
-                    <li>She said she didn’t remember getting an email from him.</li>
-                    <li>I remember meeting her once. It must have been about five years ago.</li>
-                </ul>
-                <h3>Remind</h3>
-                <p>If a person or thing reminds you of someone or something, they make you think of that person or thing, or they resemble that person or thing:</p>
-                <ul className="page-examples">
-                    <li>That song always reminds me of the time I fell in love with a Nigerian girl.</li>
-                    <li>Jason reminds me of my father. They have the same eyes and the same way of talking.</li>
-                </ul>
-                <p>If we remind someone to do something or about something, we make them remember it or help them not to forget it:</p>
-                <ul className="page-examples">
-                    <li>He’s probably forgotten he was supposed to be here at 6. Should I call and remind him?</li>
-                    <li>Remind me to email Tony today; otherwise I’ll probably forget.</li>
-                </ul>
-            
-            <p>Add a form of remember or remind</p>
-            <Quiz title="Quiz" questions={[
-                {
-                    title: "Did you  ____________________ Jeff to bring his credit card?",
-                    possibleAnswers: [
-                        {title: "remind", correctAnswer: true},
-                        {title: "remember"},
-                    ],
-                    userAnswer: "",
-                },
-                {
-                    title: "I can't ___________________ anything about the accident at all.",
-                    possibleAnswers: [
-                        {title: "remind" },
-                        {title: "remember",correctAnswer: true},
-                    ],
-                    userAnswer: "",
-                },
-                {
-                    title: "I had to ______________________ him that we had signed an agreement. He seemed to have forgotten.",
-                    possibleAnswers: [
-                        {title: "remember" },
-                        {title: "remind",correctAnswer: true},
-                    ],
-                    userAnswer: "",
-                },
-                {
-                    title: "Please ____________________ not to walk on the grass and not to pick any flowers.",
-                    possibleAnswers: [
-                        {title: "remind" },
-                        {title: "remember",correctAnswer: true},
-                    ],
-                    userAnswer: "",
-                },
-                {
-                    title: "The leader of the group ____________________ me of my best friend's brother.",
-                    possibleAnswers: [
-                        {title: "reminds", correctAnswer: true },
-                        {title: "remembers"},
-                    ],
-                    userAnswer: "",
-                },
-                {
-                    title: " ____________________to switch off the computer when you leave the office, and turn off all the lights.",
-                    possibleAnswers: [
-                        {title: "Remember" },
-                        {title: "Remind", correctAnswer: true },
-                    ],
-                    userAnswer: "",
-                },
-                {
-                    title: "I've read that book but I don't __________________ the author's name.",
-                    possibleAnswers: [
-                        {title: "remember", correctAnswer: true },
-                        {title: "remind" },
-                    ],
-                    userAnswer: "",
-                },
-                {
-                    title: "That perfume _______________ me of my grandmother. She always wore it.",
-                    possibleAnswers: [
-                        {title: "reminds", correctAnswer: true },
-                        {title: "remembers" },
-                    ],
-                    userAnswer: "",
-                },
-                {
-                    title: "_______________ me to buy flowers for my mother. It's her birthday tomorrow.",
-                    possibleAnswers: [
-                        {title: "remind", correctAnswer: true },
-                        {title: "remember" },
-                    ],
-                    userAnswer: "",
-                },
-                {
-                    title: "I don't ______________ where I left my umbrella. Have you seen it anywhere?",
-                    possibleAnswers: [
-                        {title: "remind" },
-                        {title: "remember", correctAnswer: true },
-                    ],
-                    userAnswer: "",
-                },       
-            ]}></Quiz>
-        </div>
-    )
-} 
+  //whatever
+  return (
+    <div className="page-body">
+      <div className="page-title">
+        <h2>Remember versus Remind</h2>
+      </div>
+      <h3>Remember</h3>
+      <p>
+        If we remember someone or something, we keep that person or thing in our
+        mind or we bring that person or thing back to our mind:
+      </p>
+      <ul className="page-examples">
+        <li>I’ll always remember you. You have helped me so much.</li>
+        <li>Suddenly I remembered I promised to call my mother.</li>
+      </ul>
+      <p>Remember + infinitive means that we don’t forget to do something:</p>
+      <ul className="page-examples">
+        <li>
+          Remember to back up your files, just in case your computer crashes.
+        </li>
+      </ul>
+      <p>
+        Remember + the -ing form of a verb means ‘have a memory of something we
+        did or of something that happened in the past:
+      </p>
+      <ul className="page-examples">
+        <li>She said she didn’t remember getting an email from him.</li>
+        <li>
+          I remember meeting her once. It must have been about five years ago.
+        </li>
+      </ul>
+      <h3>Remind</h3>
+      <p>
+        If a person or thing reminds you of someone or something, they make you
+        think of that person or thing, or they resemble that person or thing:
+      </p>
+      <ul className="page-examples">
+        <li>
+          That song always reminds me of the time I fell in love with a Nigerian
+          girl.
+        </li>
+        <li>
+          Jason reminds me of my father. They have the same eyes and the same
+          way of talking.
+        </li>
+      </ul>
+      <p>
+        If we remind someone to do something or about something, we make them
+        remember it or help them not to forget it:
+      </p>
+      <ul className="page-examples">
+        <li>
+          He’s probably forgotten he was supposed to be here at 6. Should I call
+          and remind him?
+        </li>
+        <li>Remind me to email Tony today; otherwise I’ll probably forget.</li>
+      </ul>
 
-
-
-
-
-
-
-
-
-
-
-
-
+      <Quiz
+        title="Take a Quiz!"
+        questions={[
+          {
+            title:
+              "Did you  ____________________ Jeff to bring his credit card?",
+            possibleAnswers: [
+              { title: "remind", correctAnswer: true },
+              { title: "remember" },
+            ],
+            userAnswer: "",
+          },
+          {
+            title:
+              "I can't ___________________ anything about the accident at all.",
+            possibleAnswers: [
+              { title: "remind" },
+              { title: "remember", correctAnswer: true },
+            ],
+            userAnswer: "",
+          },
+          {
+            title:
+              "I had to ______________________ him that we had signed an agreement. He seemed to have forgotten.",
+            possibleAnswers: [
+              { title: "remember" },
+              { title: "remind", correctAnswer: true },
+            ],
+            userAnswer: "",
+          },
+          {
+            title:
+              "Please ____________________ not to walk on the grass and not to pick any flowers.",
+            possibleAnswers: [
+              { title: "remind" },
+              { title: "remember", correctAnswer: true },
+            ],
+            userAnswer: "",
+          },
+          {
+            title:
+              "The leader of the group ____________________ me of my best friend's brother.",
+            possibleAnswers: [
+              { title: "reminds", correctAnswer: true },
+              { title: "remembers" },
+            ],
+            userAnswer: "",
+          },
+          {
+            title:
+              " ____________________to switch off the computer when you leave the office, and turn off all the lights.",
+            possibleAnswers: [
+              { title: "Remember" },
+              { title: "Remind", correctAnswer: true },
+            ],
+            userAnswer: "",
+          },
+          {
+            title:
+              "I've read that book but I don't __________________ the author's name.",
+            possibleAnswers: [
+              { title: "remember", correctAnswer: true },
+              { title: "remind" },
+            ],
+            userAnswer: "",
+          },
+          {
+            title:
+              "That perfume _______________ me of my grandmother. She always wore it.",
+            possibleAnswers: [
+              { title: "reminds", correctAnswer: true },
+              { title: "remembers" },
+            ],
+            userAnswer: "",
+          },
+          {
+            title:
+              "_______________ me to buy flowers for my mother. It's her birthday tomorrow.",
+            possibleAnswers: [
+              { title: "remind", correctAnswer: true },
+              { title: "remember" },
+            ],
+            userAnswer: "",
+          },
+          {
+            title:
+              "I don't ______________ where I left my umbrella. Have you seen it anywhere?",
+            possibleAnswers: [
+              { title: "remind" },
+              { title: "remember", correctAnswer: true },
+            ],
+            userAnswer: "",
+          },
+        ]}
+      ></Quiz>
+    </div>
+  );
+}
