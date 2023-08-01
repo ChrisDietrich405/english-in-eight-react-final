fa3e7151a8c78fd4b6247562e3c4fff9330bb3f2
Chris <chrisdietrich3666@gmail.com>
2023-08-01 10:18:11 -0400

    test

--- src/pages/versus/versus-categories/RememberVersusRemind.js
+++ src/pages/versus/versus-categories/RememberVersusRemind.js
@@ -1,3 +1,5 @@
+//DONE
+
 import Quiz from "../../../components/Quiz";
 
 export default function RememberVersusRemind() {
@@ -5,58 +7,62 @@ export default function RememberVersusRemind() {
   return (
     <div className="page-body">
       <div className="page-title">
-        <h2>Remember versus Remind</h2>
+        <h2>Remind versus Remember</h2>
       </div>
-      <h3>Remember</h3>
-      <p>
-        If we remember someone or something, we keep that person or thing in our
-        mind or we bring that person or thing back to our mind:
-      </p>
-      <ul className="page-examples">
-        <li>I’ll always remember you. You have helped me so much.</li>
-        <li>Suddenly I remembered I promised to call my mother.</li>
-      </ul>
-      <p>Remember + infinitive means that we don’t forget to do something:</p>
-      <ul className="page-examples">
-        <li>
-          Remember to back up your files, just in case your computer crashes.
-        </li>
-      </ul>
-      <p>
-        Remember + the -ing form of a verb means ‘have a memory of something we
-        did or of something that happened in the past:
-      </p>
-      <ul className="page-examples">
-        <li>She said she didn’t remember getting an email from him.</li>
-        <li>
-          I remember meeting her once. It must have been about five years ago.
-        </li>
-      </ul>
+
       <h3>Remind</h3>
       <p>
-        If a person or thing reminds you of someone or something, they make you
-        think of that person or thing, or they resemble that person or thing:
+        To remind means to cause someone to remember something or to bring a
+        memory or a specific piece of information back to their attention. It
+        involves prompting or recalling an event, task, or piece of knowledge
+        that has previously been discussed or encountered.
       </p>
       <ul className="page-examples">
         <li>
-          That song always reminds me of the time I fell in love with a Nigerian
-          girl.
+          My boss reminds me of my father. They have the same way of telling
+          jokes.
         </li>
         <li>
-          Jason reminds me of my father. They have the same eyes and the same
-          way of talking.
+          That song always reminds me of when I fell in love with a Nigerian
+          girl.
         </li>
       </ul>
       <p>
-        If we remind someone to do something or about something, we make them
-        remember it or help them not to forget it:
+        If we remind another person about something or to do something, we help
+        them remember it or help them not to forget it:
       </p>
       <ul className="page-examples">
+        <li>Remind me to take out the trash tonight. If you don't I’ll probably forget.</li>
         <li>
-          He’s probably forgotten he was supposed to be here at 6. Should I call
-          and remind him?
+          She’s probably forgotten she was supposed to be at the meeting. Someone should text her.
         </li>
-        <li>Remind me to email Tony today; otherwise I’ll probably forget.</li>
+      </ul>
+      <h3>Remember</h3>
+      <p>
+        The term "remember" refers to the mental process of recalling or
+        retaining information, experiences, or events from the past. It involves
+        the ability to retrieve things that have been previously learned,
+        encountered, or perceived.
+      </p>
+      <ul className="page-examples">
+        <li>I’ll always my trip to Jamaica.</li>
+        <li>Suddenly I remembered I needed to pay that bill.</li>
+      </ul>
+     
+      <p>
+        Remember + the -ing form of a verb describes having a memory of something that happened in the past or of something we experienced 
+      </p>
+      <ul className="page-examples">
+        <li>They said they didn't remember going to the parking lot.</li>
+        <li>
+          I remember talking to him. I think it was Friday.
+        </li>
+      </ul>
+      <p>Remember + infinitive describes a command, telling someone not to forget something</p>
+      <ul className="page-examples">
+        <li>
+          Remember to go to bed very early tonight. Tomorrow is going to be a long day. 
+        </li>
       </ul>
 
       <Quiz
@@ -64,16 +70,7 @@ export default function RememberVersusRemind() {
         questions={[
           {
             title:
-              "Did you  ____________________ Jeff to bring his credit card?",
-            possibleAnswers: [
-              { title: "remind", correctAnswer: true },
-              { title: "remember" },
-            ],
-            userAnswer: "",
-          },
-          {
-            title:
-              "I can't ___________________ anything about the accident at all.",
+              "I don't ______________ where I left my hat.",
             possibleAnswers: [
               { title: "remind" },
               { title: "remember", correctAnswer: true },
@@ -82,16 +79,7 @@ export default function RememberVersusRemind() {
           },
           {
             title:
-              "I had to ______________________ him that we had signed an agreement. He seemed to have forgotten.",
-            possibleAnswers: [
-              { title: "remember" },
-              { title: "remind", correctAnswer: true },
-            ],
-            userAnswer: "",
-          },
-          {
-            title:
-              "Please ____________________ not to walk on the grass and not to pick any flowers.",
+              "She can't ___________________ anything about her childhood.",
             possibleAnswers: [
               { title: "remind" },
               { title: "remember", correctAnswer: true },
@@ -100,7 +88,25 @@ export default function RememberVersusRemind() {
           },
           {
             title:
-              "The leader of the group ____________________ me of my best friend's brother.",
+              "She had to ______________________ him that they had agreed on the child custody. He acted like he had forgotten",
+            possibleAnswers: [
+              { title: "remember" },
+              { title: "remind", correctAnswer: true },
+            ],
+            userAnswer: "",
+          },
+          {
+            title:
+              "Please ____________________ not to smoke near the church.",
+            possibleAnswers: [
+              { title: "remind" },
+              { title: "remember", correctAnswer: true },
+            ],
+            userAnswer: "",
+          },
+          {
+            title:
+              "My new boss ____________________ me of my best friend's sister.",
             possibleAnswers: [
               { title: "reminds", correctAnswer: true },
               { title: "remembers" },
@@ -109,7 +115,7 @@ export default function RememberVersusRemind() {
           },
           {
             title:
-              " ____________________to switch off the computer when you leave the office, and turn off all the lights.",
+              " ____________________ to turn off the computer when you leave the office.",
             possibleAnswers: [
               { title: "Remember" },
               { title: "Remind", correctAnswer: true },
@@ -118,7 +124,7 @@ export default function RememberVersusRemind() {
           },
           {
             title:
-              "I've read that book but I don't __________________ the author's name.",
+              "I've read that post but I don't __________________ the who wrote it.",
             possibleAnswers: [
               { title: "remember", correctAnswer: true },
               { title: "remind" },
@@ -127,16 +133,7 @@ export default function RememberVersusRemind() {
           },
           {
             title:
-              "That perfume _______________ me of my grandmother. She always wore it.",
-            possibleAnswers: [
-              { title: "reminds", correctAnswer: true },
-              { title: "remembers" },
-            ],
-            userAnswer: "",
-          },
-          {
-            title:
-              "_______________ me to buy flowers for my mother. It's her birthday tomorrow.",
+              "Did you  ____________________ Malcolm to bring his umbrella?",
             possibleAnswers: [
               { title: "remind", correctAnswer: true },
               { title: "remember" },
@@ -145,13 +142,23 @@ export default function RememberVersusRemind() {
           },
           {
             title:
-              "I don't ______________ where I left my umbrella. Have you seen it anywhere?",
+              "That dress _______________ me of my cousin. She wore the same kind.",
             possibleAnswers: [
-              { title: "remind" },
-              { title: "remember", correctAnswer: true },
+              { title: "reminds", correctAnswer: true },
+              { title: "remembers" },
             ],
             userAnswer: "",
           },
+          {
+            title:
+              "_______________ me to buy some trash bags.",
+            possibleAnswers: [
+              { title: "remind", correctAnswer: true },
+              { title: "remember" },
+            ],
+            userAnswer: "",
+          },
+         
         ]}
       ></Quiz>
     </div>
